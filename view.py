8973db7bb94ec806d714e714091c75191d668bd1
Ermakovis <ermakov.ugra@gmail.com>
2023-03-29 11:42:48 +0500

    view add show_phonebook

--- view.py
+++ view.py
@@ -1,2 +1,10 @@
 def menu():
-    pass
+    print("1. Вывод телефонной книги")
+    print("2. Сохранение контакта")
+    print("3. Поиск по фамилии")
+    print("4. Изменение контакта")
+    print("5. Удаление контакта") 
+
+def show_phonebook(data):
+    for i in data:
+        print(i)
